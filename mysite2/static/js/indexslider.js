cff0a818c1cd1d559bd2264999a2cdba7d6845ae
darkzero <ksks0908@naver.com>
2015-02-04 22:27:23 +0900

    no message

--- mysite2/static/js/indexslider.js
+++ mysite2/static/js/indexslider.js
@@ -47,7 +47,6 @@ var ecprof1 = document.getElementsByName('cprof')[2];
 var elikenum1 = document.getElementById('likenum1');
 
 
-eccode1.textContent = document.getElementsByName('ccode')[0].value;
 ecname1.textContent = cname[1];
 ecprof1.textContent = "후";
 

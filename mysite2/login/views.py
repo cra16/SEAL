0c4a7e5cabb7dec20f215fe9b6ace144a795d3b0
darkzero <ksks0908@naver.com>
2015-02-04 15:35:26 +0900

    no message

--- mysite2/login/views.py
+++ mysite2/login/views.py
@@ -63,7 +63,7 @@ def loginCheck(request):
 		else:
 			count=Lecture.objects.count()
 
-			TotalCount = (count/8)+1
+			TotalCount = (count/6)+1
 
 			if TotalCount ==1:
 				Next = 1
@@ -71,7 +71,7 @@ def loginCheck(request):
 				Next =TotalCount
 			Previous=1
 	
-			PageBoard = Lecture.objects.order_by('-id')[0:7]	
+			PageBoard = Lecture.objects.order_by('-id')[0:5]	
 			return render_to_response("index.html",
 					  {'user':request.user,
 					   'PageBoard':PageBoard, 

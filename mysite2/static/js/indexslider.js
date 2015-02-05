fd2e1b754c0c4ae758f86b02401cc3ec01eab6b2
darkzero <ksks0908@naver.com>
2015-02-05 20:33:14 +0900

    no message

--- mysite2/static/js/indexslider.js
+++ mysite2/static/js/indexslider.js
@@ -72,6 +72,35 @@ var sa11 = $('#sa11').slider(),
     sa73 = $('#sa73').slider(),
     sa74 = $('#sa74').slider();
 
+var sk11 = $('#sk11').slider(),
+    sk12 = $('#sk12').slider(),
+    sk13 = $('#sk13').slider(),
+    sk14 = $('#sk14').slider(),
+    sk21 = $('#sk21').slider(),
+    sk22 = $('#sk22').slider(),
+    sk23 = $('#sk23').slider(),
+    sk24 = $('#sk24').slider(),
+    sk31 = $('#sk31').slider(),
+    sk32 = $('#sk32').slider(),
+    sk33 = $('#sk33').slider(),
+    sk34 = $('#sk34').slider(),
+    sk41 = $('#sk41').slider(),
+    sk42 = $('#sk42').slider(),
+    sk43 = $('#sk43').slider(),
+    sk44 = $('#sk44').slider(),
+    sk51 = $('#sk51').slider(),
+    sk52 = $('#sk52').slider(),
+    sk53 = $('#sk53').slider(),
+    sk54 = $('#sk54').slider(),
+    sk61 = $('#sk61').slider(),
+    sk62 = $('#sk62').slider(),
+    sk63 = $('#sk63').slider(),
+    sk64 = $('#sk64').slider(),
+    sk71 = $('#sk71').slider(),
+    sk72 = $('#sk72').slider(),
+    sk73 = $('#sk73').slider(),
+    sk74 = $('#sk74').slider();
+
 $('#sl11').slider('disable');
 $('#sl12').slider('disable');
 $('#sl13').slider('disable');
@@ -129,6 +158,35 @@ $('#sa71').slider('disable');
 $('#sa72').slider('disable');
 $('#sa73').slider('disable');
 $('#sa74').slider('disable');
+
+$('#sk11').slider('disable');
+$('#sk12').slider('disable');
+$('#sk13').slider('disable');
+$('#sk14').slider('disable');
+$('#sk21').slider('disable');
+$('#sk22').slider('disable');
+$('#sk23').slider('disable');
+$('#sk24').slider('disable');
+$('#sk31').slider('disable');
+$('#sk32').slider('disable');
+$('#sk33').slider('disable');
+$('#sk34').slider('disable');
+$('#sk41').slider('disable');
+$('#sk42').slider('disable');
+$('#sk43').slider('disable');
+$('#sk44').slider('disable');
+$('#skl51').slider('disable');
+$('#sk52').slider('disable');
+$('#sk53').slider('disable');
+$('#sk54').slider('disable');
+$('#sk61').slider('disable');
+$('#sk62').slider('disable');
+$('#sk63').slider('disable');
+$('#sk64').slider('disable');
+$('#sk71').slider('disable');
+$('#sk72').slider('disable');
+$('#sk73').slider('disable');
+$('#sk74').slider('disable');
 
 
 $('#sl11').slider('setValue', 5);
@@ -188,6 +246,36 @@ $('#sa71').slider('setValue', 5);
 $('#sa72').slider('setValue', 5);
 $('#sa73').slider('setValue', 5);
 $('#sa74').slider('setValue', 5);
+
+$('#sk11').slider('setValue', 5);
+$('#sk12').slider('setValue', 5);
+$('#sk13').slider('setValue', 5);
+$('#sk14').slider('setValue', 5);
+$('#sk21').slider('setValue', 5);
+$('#sk22').slider('setValue', 5);
+$('#sk23').slider('setValue', 5);
+$('#sk24').slider('setValue', 5);
+$('#sk31').slider('setValue', 5);
+$('#sk32').slider('setValue', 5);
+$('#sk33').slider('setValue', 5);
+$('#sk34').slider('setValue', 5);
+$('#sk41').slider('setValue', 5);
+$('#sk42').slider('setValue', 5);
+$('#sk43').slider('setValue', 5);
+$('#sk44').slider('setValue', 5);
+$('#sk51').slider('setValue', 5);
+$('#sk52').slider('setValue', 5);
+$('#sk53').slider('setValue', 5);
+$('#sk54').slider('setValue', 5);
+$('#sk61').slider('setValue', 5);
+$('#sk62').slider('setValue', 5);
+$('#sk63').slider('setValue', 5);
+$('#sk64').slider('setValue', 5);
+$('#sk71').slider('setValue', 5);
+$('#sk72').slider('setValue', 5);
+$('#sk73').slider('setValue', 5);
+$('#sk74').slider('setValue', 5);
+
 
 
 $("[data-toggle=tooltip]").tooltip();

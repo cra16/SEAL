f1af0e71db9541241e7a6b242fb77c5e6c6d8a04
darkzero <ksks0908@naver.com>
2015-02-06 10:45:32 +0900

    no message

--- mysite2/login/views.py
+++ mysite2/login/views.py
@@ -1,4 +1,4 @@
-<<<<<<< .merge_file_a11208
+
 # -*- coding: utf-8 -*-
 # -*- coding: euc-kr -*-
 
@@ -28,7 +28,21 @@ def Register(request):
 		return render_to_response('login.html')
 
 
-	
+# 히스넷 체크를 위한 크롤링 모듈
+from selenium import webdriver
+
+@csrf_exempt
+def Register(request):
+	if request.method=='POST':
+		user_id = request.POST['user_id']
+		user_pw = request.POST['user_pw']
+		user_email = request.POST['user_email']
+		
+		User.object.create_user(username=user_id, password=user_pw, email=user_email)
+
+		return render_to_response('login.html')
+	else:
+		return render_to_response('login.html')	
 
     
 
@@ -170,111 +184,6 @@ def logout_page(request):
 
 
     
-
-@csrf_exempt
-def Confirm(request):
-	
-	if request.method =='POST':
-		UserStuNumber = request.POST['year']
-		UserName = request.POST['username']
-		UserPassword = request.POST['userpassword']
-
-		return render_to_response('register.html')
-
-	else :
-		return render_to_response('confirm.html')
-
-
-# Create your views here.
-=======
-# -*- coding: utf-8 -*-
-# -*- coding: euc-kr -*-
-
-from login.forms import *
-from lecture.models import *
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.contrib.auth import authenticate, login as auth_login
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
-# 히스넷 체크를 위한 크롤링 모듈
-from selenium import webdriver
-
-@csrf_exempt
-def Register(request):
-	if request.method=='POST':
-		user_id = request.POST['user_id']
-		user_pw = request.POST['user_pw']
-		user_email = request.POST['user_email']
-		
-		User.object.create_user(username=user_id, password=user_pw, email=user_email)
-
-		return render_to_response('login.html')
-	else:
-		return render_to_response('login.html')
-
-@csrf_exempt
-def loginCheck(request):
-		if request.method == 'POST':
-			username = request.POST['UserID']
-			userpassword = request.POST['UserPassword']
-			user = authenticate(username = username, password=userpassword)
-			if user is not None:
-				auth_login(request,user)
-				count=Lecture.objects.count()
-
-				TotalCount = (count/8)+1
-
-				if TotalCount ==1:
-					Next = 1
-				else:
-					Next =TotalCount
-				Previous=1
-	
-				PageBoard = Lecture.objects.order_by('-id')[0:7]	
-				return render_to_response("index.html",
-					  {'user':request.user,
-					   'PageBoard':PageBoard, 
-					   'TotalCount' : range(0,TotalCount), 
-					   'Previous' : Previous, 
-					   'Next' : Next,
-					   })
-			else:
-				return render_to_response('login.html')
-         
-		elif request.user.username =="":
-			return render_to_response('login.html')
-		else:
-			count=Lecture.objects.count()
-
-			TotalCount = (count/6)+1
-
-			if TotalCount ==1:
-				Next = 1
-			else:
-				Next =TotalCount
-			Previous=1
-	
-			PageBoard = Lecture.objects.order_by('-id')[0:5]	
-			return render_to_response("index.html",
-					  {'user':request.user,
-					   'PageBoard':PageBoard, 
-					   'TotalCount' : range(0,TotalCount), 
-					   'Previous' : Previous, 
-					   'Next' : Next,
-					   })
-
-def login(request):
-    return render_to_response('login.html')
-    
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect('/mysite2')
-
 @csrf_exempt
 def Confirm(request):
 	return render_to_response('confirm.html')
@@ -338,4 +247,3 @@ def HisnetCheck(request):
 
 
 # Create your views here.
->>>>>>> .merge_file_a05500
--- mysite2/mysite2/urls.py
+++ mysite2/mysite2/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< .merge_file_a12248
+
 from django.conf.urls import patterns, include, url
 
 from login.views import *
@@ -29,7 +29,6 @@ urlpatterns = patterns('',
 	url(r'^Notice/$',NoticeMain),
 	url(r'^Notice/Page/(\d+)$',Notice),
 	url(r'^Notice/(\d+)/$',Notice_Read),
-	url(r'^Confirm/$',Confirm),
 	url(r'^Register/$',Register),
 	url(r'^FirstMajorPage/(\d+)/$',Main),
 	url(r'^SecondMajorPage/(\d+)/$',Main),
@@ -39,45 +38,7 @@ urlpatterns = patterns('',
 #	url(r'^loginCheck/$', ),
 #	url(r'^home/$', loginCheck)
 	url(r'^update/$','lecture.views.lec_update'),
-
-)
-=======
-from django.conf.urls import patterns, include, url
-
-from login.views import *
-from index.views import *
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite2.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-	url(r'^admin/', include(admin.site.urls)), 
-	url(r'^$', loginCheck),
-	url(r'^Logout/$', logout_page),
-	url(r'^accouts/login/$', 'django.contrib.auth.views.login'),
-#	url(r'^register/$', register),
-#	url(r'^register/success/$', register_success),
-	url(r'^MyPage/$', MyPage), 
-    url(r'^About/$',About),
-    url(r'^Schedule/$',Schedule),
-    url(r'^Judgement/$',Judgement),
-	url(r'^Recommend/$',Recommend),
-	url(r'^QnA/page/(\d+)$',QnA),
-	url(r'^QnA/$',QnAMain),
-	url(r'^QnA/Write/$',QnAWrite),
-	url(r'^Course/$',Course),
-	url(r'^QnA/(\d+)/$',QnARead),
-	url(r'^Notice/$',NoticeMain),
-	url(r'^Notice/Page/(\d+)$',Notice),
-	url(r'^Notice/(\d+)/$',Notice_Read),
 	url(r'^Confirm/$',Confirm),
 	url(r'^HisnetCheck/$',HisnetCheck),
-	url(r'^Register/$',Register),
-#	url(r'^loginCheck/$', ),
-#	url(r'^home/$', loginCheck)
-	url(r'^update/$','lecture.views.lec_update'),
 
 )
->>>>>>> .merge_file_a04180

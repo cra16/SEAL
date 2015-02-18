3c84f7fc6215e07d6e3f8f826b72e49e7065f60f
darkzero <ksks0908@naver.com>
2015-02-18 22:01:58 +0900

    잠깐 리셋좀함.. 대문자를 소문자로 해야해서..

--- mysite2/Notice/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from notice.models import *
-
-admin.site.register(Notice_Board)
-# Register your models here.
--- mysite2/QnA/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from qna.models import *
-
-admin.site.register(QnA_Board)
-# Register your models here.

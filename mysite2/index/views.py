1026128a4abe61339efbb44b59a6f3abf55e9b72
임현우 <ksks0908@naver.com>
2015-07-03 15:09:39 +0900

    메인페이지 비동기 완료(아직 에러없음)

--- mysite2/index/views.py
+++ mysite2/index/views.py
@@ -34,88 +34,79 @@ def Judgement(request): # 신고 게시판 기능
 	CheckingLogin(request.user.username)
 	return render_to_response("subscribe_report.html",{'user':request.user})
 
-
-def Main(request, offset): #Main 기능
+@csrf_exempt
+def Page(request): #Main 기능
 	CheckingLogin(request.user.username)	
 
 	try:
-		offset = int(offset)
+			if request.POST['Page'] !="0":
+					cur_page = int(request.POST['Page'])
+			else:
+					cur_page = 1
+			Current = request.POST['Current']
 	except ValueError:
-		raise Http404() 
+			raise Http404() 
 
-	PageInformation=request.session['PageInformation']
-	#main 자바스크립트 조작 하기 위한 기능
-	Active = ["","",""]
-	URL_Path = request.path	#현재 페이지 URL 긁어오기
-
-
-	##강의 DB에 저장된 자료들을 코드로 필터를 해서 Total Page를 만듦
-	##그리고 나서 강의추천된 강의들만 따로 또 필터해서 데이터 넣는 과정임
-	##후배여러분이 좀 알고리즘 잘짜서 더 최적화해주세요..(최대한 했지만..발적화임..))
+	target = TargetTemplate(Current)
+	template = MainPageView(request.user, request.session['PageInformation'],cur_page,int(target[1]))
+	request.session['PageInformation'] = template['PageInformation']
 	
-	CourseCode = MajorSelect(request.user)
-	
-	#2차원 list로 각 전공당 총 페이지 수 저장
-	T_Count=[[] ,[] ,[]]
-	if CourseCode[0] !="ENG":
-			DBCount1=Lecture.objects.filter(Q(Code__contains=CourseCode[0]) | Q(Code__contains= CourseCode[1])).count()
-			DBCount2=Lecture.objects.filter(Q(Code__contains= CourseCode[2]) | Q(Code__contains=CourseCode[3])).count()
-			Condition1= (DBCount1%5!=0) and 1 or 0
-			Condition2= (DBCount2%5!=0) and 1 or 0
-			T_Count[0] = DBCount/5+Condition1
-			T_Count[1] = DBCount/5+Condition2
-	else:
-			DBCount1=Lecture.objects.filter(Q(Code__contains=CourseCode[0]) | Q(Code__contains= CourseCode[1]) | Q(Code__contains=CourseCode[2])| Q(Code__contains=CourseCode[3]) | Q(Code__contains=CourseCode[4]) | Q(Code__contains=CourseCode[5])).count()/5+1
-			DBCount2=Lecture.objects.filter(Q(Code__contains=CourseCode[0]) | Q(Code__contains= CourseCode[1]) | Q(Code__contains=CourseCode[2])| Q(Code__contains=CourseCode[3]) | Q(Code__contains=CourseCode[4]) | Q(Code__contains=CourseCode[5])).count()/5+1
-			Condition1= (DBCount1%5!=0) and 1 or 0
-			Condition2= (DBCount2%5!=0) and 1 or 0
-			T_Count[0] = DBCount1/5+Condition1
-			T_Count[1] = DBCount2/5+Condition2
-	T_Count[2] = Lecture.objects.count()/5+1
-	
-	#main 페이지 활성화 기능(1전공, 2전공 all)
-	Active = ["","",""]
-	ActiveCount=0
-	#URL을 통해 무슨 전공 페이지에 있는지 확인해서 그 정보 긁어옴
-	PageData= ["FirstMajorPage","SecondMajorPage","AllPage"]
-	for i in range(0,len(T_Count)):
-		if URL_Path.find(PageData[i]) != -1:
-			#페이지 갯수가 11개 이상일 경우
-			PageInformation[i]=CurrentPageView(T_Count,offset,i)
-			PageInformation[i][1] = offset
-			Active[i] = "active" # main 페이지
-			ActiveCount+=1
-		else:
-			pass
-	if ActiveCount ==0:
-		Active=None
-	#각 강의 전공에 해당하는 DB 정보 저장 함 
-	TotalBoard = [[],[],[]]
-	if CourseCode[0] !="ENG":
-		TotalBoard[0] = Lecture.objects.filter(Q(Code__contains = CourseCode[0]) | Q(Code__contains=CourseCode[1])).order_by('Code')[(PageInformation[0][1]-1)*5:(PageInformation[0][1]-1)*5+5]
-		TotalBoard[1] = Lecture.objects.filter(Q(Code__contains = CourseCode[2]) | Q(Code__contains=CourseCode[3])).order_by('Code')[(PageInformation[1][1]-1)*5:(PageInformation[1][1]-1)*5+5]
-	else:
-		TotalBoard[0] = Lecture.objects.filter(Q(Code__contains =CourseCode[0]) |Q(Code__contains=CourseCode[1])|Q(Code__contains=CourseCode[2])|Q(Code__contains=CourseCode[3])|Q(Code__contains=CourseCode[4])|Q(Code__contains=CourseCode[5])).order_by('Code')[(PageInformation[0][1]-1)*5:(PageInformation[0][1]-1)*5+5]
-		TotalBoard[1] = Lecture.objects.filter(Q(Code__contains =CourseCode[2]) |Q(Code__contains=CourseCode[3])|Q(Code__contains=CourseCode[2])|Q(Code__contains=CourseCode[3])|Q(Code__contains=CourseCode[4])|Q(Code__contains=CourseCode[5])).order_by('Code')[(PageInformation[1][1]-1)*5:(PageInformation[1][1]-1)*5+5]
-	TotalBoard[2] = Lecture.objects.order_by('Code')[(PageInformation[2][1]-1)*5:(PageInformation[2][1]-1)*5+5]
+	return render_to_response(target[0],template)
+@csrf_exempt
+def FirstPage(request):
+	CheckingLogin(request.user.username)	
 
-	PageBoard = PageView(TotalBoard)
-	##Session Save
-	request.session['PageInformation'] = PageInformation
+	try:
+			if request.POST['Page'] !="0":
+					cur_page = int(request.POST['Page'])
+			else:
+					cur_page = 1
+			Current = request.POST['Current']
+	except ValueError:
+			raise Http404() 
 
-	# 페이지 총 수(페이지 넘길 때)
-	TotalCount=list()
-	for i in range(0,len(T_Count)):
-		TotalCount.append(PageTotalCount(i,T_Count,PageInformation[i]))
-	return render_to_response("index.html",
-		  {'user':request.user,
-		   'PageBoard':PageBoard,
-		   'TotalCount' : TotalCount,
-		   'PageInformation' : PageInformation,
-		   'Path':URL_Path,
-		   'Active':Active,
-		   'T_Count':T_Count
-		   })
+	target = TargetTemplate(Current)
+	template = MainPageView(request.user, request.session['PageInformation'],cur_page,int(target[1]))
+	request.session['PageInformation'] = template['PageInformation']
+	
+	return render_to_response(target[0],template)
+@csrf_exempt
+def SecondPage(request):
+	CheckingLogin(request.user.username)	
+
+	try:
+			if request.POST['Page'] !="0":
+					cur_page = int(request.POST['Page'])
+			else:
+					cur_page = 1
+			Current = request.POST['Current']
+	except ValueError:
+			raise Http404() 
+
+	target = TargetTemplate(Current)
+	template = MainPageView(request.user, request.session['PageInformation'],cur_page,int(target[1]))
+	request.session['PageInformation'] = template['PageInformation']
+	
+	return render_to_response(target[0],template)
+
+@csrf_exempt
+def ThirdPage(request):
+	CheckingLogin(request.user.username)	
+
+	try:
+			if request.POST['Page'] !="0":
+					cur_page = int(request.POST['Page'])
+			else:
+					cur_page = 1
+			Current = request.POST['Current']
+	except ValueError:
+			raise Http404() 
+
+	target = TargetTemplate(Current)
+	template = MainPageView(request.user, request.session['PageInformation'],cur_page,int(target[1]))
+	request.session['PageInformation'] = template['PageInformation']
+	
+	return render_to_response(target[0],template)
 
 def SubScript(request):
 	CheckingLogin(request.user.username)
@@ -205,144 +196,9 @@ def SearchPage(request, offset):
 										'PageInformation' : PageInformation,
 											'T_Count' : T_Count,
 									})
-#메인 페이지 view 함수로 옮김
-def PageView(TotalBoard):
-	PageBoard=[[],[],[]]
-	count =0
-	for DBBoard in TotalBoard:
-		for Board in DBBoard:
-			try:
-				# 총 강의 추천된 DB 강의 명으로 호출
-				BoardData = Total_Evaluation.objects.get(Course=Board)
-			except :
-				BoardData = None
 
-			if BoardData is not None:
-				BoardData.Total_Speedy = BoardData.Total_Speedy/BoardData.Total_Count
-				BoardData.Total_Reliance = BoardData.Total_Reliance/BoardData.Total_Count
-				BoardData.Total_Helper = BoardData.Total_Helper/BoardData.Total_Count
-				BoardData.Total_Question = BoardData.Total_Question/BoardData.Total_Count
-				BoardData.Total_Exam = BoardData.Total_Exam/BoardData.Total_Count
-				BoardData.Total_Homework = BoardData.Total_Homework/BoardData.Total_Count
-				PageBoard[count].append(BoardData)
-			else:
-				BoardData = Total_Evaluation(Course=Board)
-				BoardData.Total_Speedy =5
-				BoardData.Total_Reliance =5
-				BoardData.Total_Helper = 5
-				BoardData.Total_Question = 5
-				BoardData.Total_Exam = 5
-				BoardData.Total_Homework = 5
-				BoardData.Total_Count =0
-				PageBoard[count].append(BoardData)
-		count=count+1
-	return PageBoard
 
                                                                   
-def MajorSelect(user):
-		try:
-			Student = Profile.objects.get(User =user)
-		except:
-			return None
-		MajorCode= [[],[],[],[],[],[]]	
-		FirstMajor = str(Student.FirstMajor)
-		SecondMajor = str(Student.SecondMajor)
-			#1전공 선택 하는 조건문
-		if FirstMajor.find("국제") != -1 or FirstMajor.find("영어") != -1:
-			MajorCode[0]="ISE"
-			MajorCode[1]="None"
-		elif FirstMajor.find("경영학") != -1 or FirstMajor.find("경제학")!= -1:
-			MajorCode[0]="GMP"
-			MajorCode[1]="MEC"
-		elif FirstMajor.find("한국법") != -1 or FirstMajor.find("UIL")!= -1:
-			MajorCode[0]="LAW"
-			MajorCode[1]="UIL"
-		elif FirstMajor.find("공연영상")!= -1 or FirstMajor.find("언로정보학")!= -1:
-			MajorCode[0]="CCC"
-			MajorCode[1]="None"
-		elif FirstMajor.find("건설공학") != -1 or FirstMajor.find("도시환경")!= -1:
-			MajorCode[0]="CUE"
-			MajorCode[1]="None"
-		elif FirstMajor.find("기계공학")!= -1 or FirstMajor.find("전자제어")!= -1 or FirstMajor.find("기전공학")!=-1:
-			MajorCode[0]="HMM"
-			MajorCode[1]="None"
-		elif FirstMajor.find("시각디자인")!= -1 or FirstMajor.find("제품디자인")!= -1:
-			MajorCode[0]="IID"
-			MajorCode[1]="None"
-		elif FirstMajor.find("생명과학")!= -1:
-			MajorCode[0]="BFT"
-			MajorCode[1]="None"
-		elif FirstMajor.find("컴퓨터") != -1 or FirstMajor.find("전자") != -1:
-			MajorCode[0]="ECE"
-			MajorCode[1]="ITP"
-		elif FirstMajor.find("상담심리") != -1 or FirstMajor.find("사회복지")!= -1:
-			MajorCode[0]="CSW"
-			MajorCode[1]="None"
-		elif FirstMajor.find("에디슨")!= -1 :
-			MajorCode[0]="GEA"
-			MajorCode[1]="None"
-		elif FirstMajor.find("영어학과")!= -1 or FirstMajor.find("경영학과")!= -1 or FirstMajor.find("사회복지학과")!= -1:
-			MajorCode[0]="SIE"
-			MajorCode[1]="None"
-		elif FirstMajor==None :
-			MajorCode[2]="None"
-			MajorCode[3]="None"
-			MajorCode[4]="None"
-			MajorCode[5]="None"
-		else:
-			MajorCode[0]="ENG"
-			MajorCode[1]="GEK"
-			MajorCode[2]="GCS"
-			MajorCode[3]="PCO"
-			MajorCode[4]="ISL"
-			MajorCode[5]="PST"
 
-		if SecondMajor.find("국제")!=-1 or SecondMajor.find("영어")!=-1:
-			MajorCode[2]="ISE"
-			MajorCode[3]="None"
-		elif SecondMajor.find("경영학")!=-1 or SecondMajor.find("경제학")!=-1:
-			MajorCode[2]="GMP"
-			MajorCode[3]="MEC"
-		elif SecondMajor.find("한국법")!=-1 or SecondMajor.find("UIL")!=-1:
-			MajorCode[2]="LAW"
-			MajorCode[3]="UIL"
-		elif SecondMajor.find("공연영상") !=-1 or SecondMajor.find("언로정보학")!=-1:
-			MajorCode[2]="CCC"
-			MajorCode[3]="None"
-		elif SecondMajor.find("건설공학") !=-1 or SecondMajor.find("도시환경")!=-1:
-			MajorCode[2]="CUE"
-			MajorCode[3]="None"
-		elif SecondMajor.find("기계공학")!=-1 or SecondMajor.find("전자제어")!=-1 or SecondMajor.find("기전공학")!=-1:
-			MajorCode[2]="HMM"
-			MajorCode[3]="None"
-		elif SecondMajor.find("시각디자인")!=-1 or SecondMajor.find("제품디자인")!=-1:
-			MajorCode[2]="IID"
-			MajorCode[3]="None"
-		elif SecondMajor.find("생명과학")!=-1:
-			MajorCode[2]="BFT"
-			MajorCode[3]="None"
-		elif SecondMajor.find("컴퓨터")!=-1 or SecondMajor.find("전자")!=-1:
-			MajorCode[2]="ECE"
-			MajorCode[3]="ITP"
-		elif SecondMajor.find("상담심리")!=-1 or SecondMajor.find("사회복지")!=-1:
-			MajorCode[2]="CSW"
-			MajorCode[3]="None"
-		elif SecondMajor.find("에디슨")!=-1:
-			MajorCode[2]="GEA"
-			MajorCode[3]="None"
-		elif SecondMajor.find("영어학과") !=-1 or SecondMajor.find("경영학과") !=-1 or SecondMajor.find("사회복지학과")!=-1:
-			MajorCode[2]="SIE"
-			MajorCode[3]="None"
-		elif SecondMajor==None :
-			MajorCode[2]="None"
-			MajorCode[3]="None"
-			MajorCode[4]="None"
-			MajorCode[5]="None"
-		else:
-			MajorCode[2]="GCS"
-			MajorCode[3]="PCO"
-			MajorCode[4]="ISL"
-			MajorCode[5]="PST"
-		return MajorCode
 
 # Create your views here
--- mysite2/static/js/IndexPage/main.js
+++ mysite2/static/js/IndexPage/main.js
@@ -18,6 +18,183 @@ $(document).ready(function() {
   });
     
     $('.fullscreen.modal').modal('show');
-    
+    /*
+    $('div').on('click','#FirstMajor',function(){
+      event.stopPropagation();
+      $(this).unbind("click");
+        $.ajax(
+            { url : "/mysite2/FirstPage/",
+              data : {
+                      'Page': "0"
+                     },
+              datatype : "json",
+              type : "POST",
+
+              success:function(resp){
+                  
+                  $('#FirstPage').html(resp);
+                     
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          });
+      });
+    $('div').on('click','#SecondMajor',function(){
+      event.stopPropagation();
+          $(this).unbind("click");
+        $.ajax(
+            { url : "/mysite2/SecondPage/",
+              data : {
+                           'Page': "0"
+                    },
+              type : "POST",
+              success:function(resp){      
+
+                  $('#SecondPage').html(resp);
+                     
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          });
+      });
+
+      $('div').on('click','#All',function(){
+        event.stopPropagation();
+          $(this).unbind("click");
+        $.ajax(
+            { url : "/mysite2/ThirdPage/",
+              data : {'Page': "0"
+                    },
+              datatype : "json",
+              type : "POST",
+              success:function(resp){         
+                  $('#ThirdPage').html(resp);
+     
+                     
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          });
+        });
+*/
+
+        $('div').on('click',"#Page",function(event){
+          event.stopPropagation();
+          $(this).unbind("click");
+          var CurrentPage=$(this).parent().attr("id")
+
+        $.ajax(
+            { url : "/mysite2/Page/",
+              data : {'Page': $(this).attr("name"),
+                      'Current':CurrentPage
+                    },
+              
+              datatype:"json",
+              type : "POST",
+              async:true,
+              success:function(resp){     
+                  if(CurrentPage =="FirstPageNation")
+                       $('#FirstPage').html(resp);
+                  else if(CurrentPage =="SecondPageNation")
+                        $('#SecondPage').html(resp);
+                  else
+                        $('#ThirdPage').html(resp);
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          
+          });
+
+     
+
+
+
+    });
+
+    $('div').on('click','#Next',function(){
+        event.stopPropagation();
+          $(this).unbind("click");
+          var CurrentPage=$(this).parent().attr("id")
+
+        $.ajax(
+            { url : "/mysite2/Page/",
+              data : {'Page': $(this).attr("name"),
+                      'Current':CurrentPage
+                    },
+              
+              datatype:"json",
+              type : "POST",
+              async:true,
+              success:function(resp){     
+                  if(CurrentPage =="FirstPageNation")
+                       $('#FirstPage').html(resp);
+                  else if(CurrentPage =="SecondPageNation")
+                        $('#SecondPage').html(resp);
+                  else
+                        $('#ThirdPage').html(resp);
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          });
+
+
+
+    });
+
+    $('div').on('click','#Previous',function(){
+        event.stopPropagation();
+          $(this).unbind("click");
+          var CurrentPage=$(this).parent().attr("id")
+
+        $.ajax(
+            { url : "/mysite2/Page/",
+              data : {'Page': $(this).attr("name"),
+                      'Current':CurrentPage
+                    },
+              
+              datatype:"json",
+              type : "POST",
+              async:true,
+              success:function(resp){     
+                  if(CurrentPage =="FirstPageNation")
+                       $('#FirstPage').html(resp);
+                  else if(CurrentPage =="SecondPageNation")
+                        $('#SecondPage').html(resp);
+                  else
+                        $('#ThirdPage').html(resp);
+                },
+                error: function(xhr, option, error){
+                  alert(xhr.status); //오류코드
+                  alert(error); //오류내용
+
+                  } 
+            
+          });
+
+
+
+    });
+
 });
+
 

887afd5c2d2341509f99e25e9ad02c63092252c7
Yangyangii <yjh2067@gmail.com>
2016-02-21 22:58:02 +0900

    오타 정정으로 해결

--- mysite2/static/js/recommend.js
+++ mysite2/static/js/recommend.js
@@ -132,6 +132,18 @@ for(var i=0; i<ccode.length; i++)
     $("#StarValue").val(starcount);
   });
 
+  $("#recommend_form").submit(function(){
+    var HSemester = $("#HSemester").val();
+    //check if there is nothing
+    if(HSemester == "0" || HSemester == ""){
+      window.scrollTo(0, 200);
+      alert("Please select the semester.");
+      $("#text_semester").css("border", "2px solid red");
+      return false;
+    } else {
+      $('#recommend_form').attr({action:'/Recommend/Recommend_Write'}).submit();
+    }
+  });
 });
 
 

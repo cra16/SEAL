dfbf8b643590f57bca775fbd7b7a39a1455e2989
darkzero <ksks0908@naver.com>
2017-04-18 14:34:10 +0900

    table register

--- mysite2/index/admin.py
+++ mysite2/index/admin.py
@@ -5,5 +5,6 @@ admin.site.register(Description_Answer)
 admin.site.register(Course_Evaluation)
 admin.site.register(Total_Evaluation)
 admin.site.register(Group_Total_Evaluation)
+admin.site.register(CountTable)
 
 # Register your models here.
